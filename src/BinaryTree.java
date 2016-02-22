cbf5738612ba21ff534a013a3d33a750b4143125
anflorea <floreaandreimihai@gmail.com>
2016-02-22 15:12:32 +0200

    Started work on Binary Tree

---
+++ src/BinaryTree.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Flo on 22-Feb-16.
+ */
+public class BinaryTree {
+
+    static class Node {
+        Node left;
+        Node right;
+        int value;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public void addNode(Node node, int value) {
+
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(8);
+    }
+}
