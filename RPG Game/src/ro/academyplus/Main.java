9b2a1d5c84d64f479babeeb6fd7b66a8888d0769
anflorea <floreaandreimihai@gmail.com>
2016-02-25 15:04:38 +0200

    Testing stuff

--- RPG Game/src/ro/academyplus/Main.java
+++ RPG Game/src/ro/academyplus/Main.java
@@ -13,6 +13,8 @@ public class Main {
         Sword sword20 = new Sword("Sword", 20);
         alissa.addArmor(armor150);
         alissa.addSword(sword20);
+        System.out.println(alissa);
+        System.out.println();
 
         System.out.println(alissa);
         System.out.println(goblin);
